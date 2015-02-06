5de9fc2dbf6a3199e104a6558867e84ed6b47dd9
Ronak Mehta <ronakrm@gmail.com>
2015-02-05 20:05:58 -0600

    Almost reads in images. Having datatype issues with insufficient memory for one image. Probably easy fix, need to set datatype somewhere. Will figure out tomorrow (Friday).

--- hdrMaker.m
+++ hdrMaker.m
@@ -1,6 +1,33 @@
 % below is hdr code. works 100% of the time all the time
 
 % read in the input file (TODO::: pass in the file name)
-file = fopen('inputs/imageInfo.txt', 'r');
-a = textscan(file, '%s', 'Delimiter', '\n');
-fclose(file);
+filename = 'inputs/imageInfo.txt';
+[imagefiles, fstops] = textread(filename, '%s %s');
+
+%would be nice to auto get this, but too much work right now
+img_width = 1024;
+img_height = 768;
+
+numimgs = size(imagefiles);
+
+images = zeros(numimgs);
+
+%%OVERFLOWS MEMORY, NOT SURE WHY
+%%maybe because using some weird data type, we only need/want uint8 (8
+%%bits)
+for i = 1:numimgs    
+    
+    i
+    %prealloc image space
+    images(i) = zeros(img_width*img_height);
+    
+    %append file directory loc
+    imageloc = strcat('inputs/',imagefiles(i));
+    
+    %read in this image
+    imgmatrix = imread(char(imageloc));
+    
+    %store in big images matrix
+    images(i) = imgmatrix;
+end
+
